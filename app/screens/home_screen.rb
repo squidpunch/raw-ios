781ba8c3679c36adcb2afd9fd0a24974c395bdb0
David Larrabee <david.larrabee@meyouhealth.com>
2015-01-17 09:34:08 -0500

    Home screen prototype

--- app/screens/home_screen.rb
+++ app/screens/home_screen.rb
@@ -1,36 +1,15 @@
 class HomeScreen < PM::Screen
-  title "Your title here"
+  title "Race Around Waltham"
   stylesheet HomeStylesheet
 
   def on_load
-    set_nav_bar_button :left, system_item: :camera, action: :nav_left_button
-    set_nav_bar_button :right, title: "Right", action: :nav_right_button
-
-    @hello_world = append!(UILabel, :hello_world)
+    append(UIButton, :about_button)
+    append(UIButton, :faq_button)
+    append(UIButton, :rules_button)
+    append(UIButton, :races_button)
   end
 
-  def nav_left_button
-    mp 'Left button'
-  end
-
-  def nav_right_button
-    mp 'Right button'
-  end
-
-  # Remove the following if you're only using portrait
-
-  # You don't have to reapply styles to all UIViews, if you want to optimize, another way to do it
-  # is tag the views you need to restyle in your stylesheet, then only reapply the tagged views, like so:
-  #   def logo(st)
-  #     st.frame = {t: 10, w: 200, h: 96}
-  #     st.centered = :horizontal
-  #     st.image = image.resource('logo')
-  #     st.tag(:reapply_style)
-  #   end
-  #
-  # Then in will_animate_rotate
-  #   find(:reapply_style).reapply_styles#
   def will_animate_rotate(orientation, duration)
-    find.all.reapply_styles
+    reapply_styles
   end
 end
--- app/stylesheets/home_stylesheet.rb
+++ app/stylesheets/home_stylesheet.rb
@@ -9,12 +9,33 @@ class HomeStylesheet < ApplicationStylesheet
     st.background_color = color.white
   end
 
-  def hello_world(st)
-    st.frame = {top: 100, width: 200, height: 18, centered: :horizontal}
-    st.text_alignment = :center
-    st.color = color.battleship_gray
-    st.font = font.medium
-    st.text = 'Hello World'
+  def about_button(st)
+    st.frame = { t: 100, w: 200, h: 25, centered: :horizontal }
+    st.text = "About"
+    button_defaults(st)
   end
 
+  def faq_button(st)
+    st.frame = { bp: 5, w: 200, h: 25, centered: :horizontal }
+    st.text = "FAQ"
+    button_defaults(st)
+  end
+
+  def rules_button(st)
+    st.frame = { bp: 5, w: 200, h: 25, centered: :horizontal }
+    st.text = "Rules"
+    button_defaults(st)
+  end
+
+  def races_button(st)
+    st.frame = { bp: 5, w: 200, h: 25, centered: :horizontal }
+    st.text = "Races"
+    button_defaults(st)
+  end
+
+  def button_defaults(st)
+    st.color = color.white
+    st.background_color = color.red
+    st.corner_radius = 6
+  end
 end
