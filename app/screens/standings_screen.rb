e793f76f63265feda70e720f72b624adaef13581
David Larrabee <david.larrabee@meyouhealth.com>
2015-02-16 20:12:07 -0500

    Adds ability to filter results

--- app/screens/standings_screen.rb
+++ app/screens/standings_screen.rb
@@ -3,6 +3,18 @@ class StandingsScreen < PM::TableScreen
   stylesheet StandingsScreenStylesheet
 
   def on_load
+    self.navigationItem.titleView = filter_control
+  end
+
+  def filter_control
+    @filter_control ||= UISegmentedControl.alloc.initWithItems(["All", "Women", "Men"]).tap do |c|
+      c.addTarget(self, action: :reload, forControlEvents: UIControlEventValueChanged)
+      c.selectedSegmentIndex = 0
+    end
+  end
+
+  def reload
+    update_table_data
   end
 
   def table_data
@@ -20,7 +32,26 @@ class StandingsScreen < PM::TableScreen
     reapply_styles
   end
 
+  def filter_value
+    case filter_control.selectedSegmentIndex
+    when 0
+      nil
+    when 1
+      "F"
+    when 2
+      "M"
+    end
+  end
+
   def results
+    if filter_value
+      all_data.select { |item| item.gender == filter_value }
+    else
+      all_data
+    end
+  end
+
+  def all_data
     [
       new_result(
         name: "Mark D'Amico",
